318b9eba1516c44c358351dd9d17888fb7a80cdd
Ricardo Casares <ricardocasares@gmail.com>
2023-02-06 03:03:51 +0100

    fix: openapi formatting

--- src/spec/generate.ts
+++ src/spec/generate.ts
@@ -1,8 +1,9 @@
+import { format } from "prettier";
 import { join } from "node:path";
 import { writeFileSync } from "node:fs";
 import { document } from "./openapi";
 
 writeFileSync(
   join(process.cwd(), "openapi.json"),
-  JSON.stringify(document, null, 2)
+  format(JSON.stringify(document), { parser: "json" })
 );
