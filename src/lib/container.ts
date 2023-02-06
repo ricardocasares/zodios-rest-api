e89f0200119c6a41ae18570915a3cb332480e02a
Ricardo Casares <ricardocasares@gmail.com>
2023-02-06 01:11:55 +0100

    fix: dependency container safety

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -2,7 +2,7 @@ export class Container<C> {
   constructor(private container: C) {}
 
   get<K extends keyof C>(key: K) {
-    return this.container[key] as C[K];
+    return this.container[key];
   }
 
   set<K extends keyof C>(key: keyof C, value: C[K]) {
